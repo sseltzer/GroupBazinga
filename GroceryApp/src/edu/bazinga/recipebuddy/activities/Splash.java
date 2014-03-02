f9c34aaff5a2f5c2dfe8d35a7709f40d00298d69
Gus Maturana <gam11f@my.fsu.edu>
2014-03-02 13:55:51 -0500

    Changes to program

--- GroceryApp/src/edu/bazinga/recipebuddy/activities/Splash.java
+++ GroceryApp/src/edu/bazinga/recipebuddy/activities/Splash.java
@@ -5,15 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import edu.bazinga.recipebuddy.R;
+import android.widget.ImageView;
+import android.graphics.drawable.AnimationDrawable;
+
 
 public class Splash extends Activity {
 
-  private static int INTRO_TIME_OUT = 500;
+  private static int INTRO_TIME_OUT = 3600;
   
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+		
+	final ImageView splashImageView = (ImageView)findViewById(R.id.splashImageView);
+	splashImageView.setBackgroundResource(R.drawable.splashanimation);
+	
+	final AnimationDrawable animation = (AnimationDrawable)splashImageView.getBackground();
+	
+	// Displays the animation on the splash screen
+	
+	splashImageView.post(new Runnable(){
+        @Override
+        public void run() {
+            animation.start();                
+        }            
+    }); 
 		
     new Handler().postDelayed(new Runnable() {
       @Override
